179362478e97857bfdd77f58681448b09979f03b
Liza <bliza0812@gmail.com>
2020-05-19 15:09:13 +0400

    Answer

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
+        Scanner in = new Scanner(System.in);
+        int A = in.nextInt();
+        System.out.println(A+(2-(A%2)));
+        in.close();
     }
 }
